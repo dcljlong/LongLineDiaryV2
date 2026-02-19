c07f1ffaabda516fe1bc44e1f88af56bee5c1e3e
David <dcljlong@gmail.com>
2026-02-20 08:58:32 +1300

    chore: deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/LongLineDiaryV2/",
   server: {
     host: "::",
     port: 8080,
